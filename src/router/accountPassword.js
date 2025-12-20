196760f56b8f0bf4574f4511c14c5eab1d867b69
Rizky Ajie Kurniawan <86877370+rizkybor@users.noreply.github.com>
2025-12-20 16:31:19 +0700

    checkpoint

---
+++ src/router/accountPassword.js
@@ -0,0 +1,33 @@
+import AccountPasswordDashboard from '@/views/admin/account-password/AccountPasswordDashboard.vue';
+// import PayrollCreate from '@/views/admin/payroll/PayrollCreate.vue';
+// import PayrollDetail from '@/views/admin/payroll/PayrollDetail.vue';
+// import MyPayslips from '@/views/employee/MyPayslips.vue';
+// import PayslipDetail from '@/views/employee/PayslipDetail.vue';
+
+export default [
+    {
+        path: 'account-password',
+        name: 'admin.account-password.dashboard',
+        component: AccountPasswordDashboard,
+    },
+    // {
+    //     path: 'payroll/create',
+    //     name: 'admin.payroll.create',
+    //     component: PayrollCreate,
+    // },
+    // {
+    //     path: 'payroll/:id',
+    //     name: 'admin.payroll.detail',
+    //     component: PayrollDetail,
+    // },
+    // {
+    //     path: 'my-payslips',
+    //     name: 'employee.payslips',
+    //     component: MyPayslips,
+    // },
+    // {
+    //     path: 'my-payslips/:id',
+    //     name: 'employee.payslips.detail',
+    //     component: PayslipDetail,
+    // }
+];
